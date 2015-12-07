db903d4bc78bfbc5885ac0c6a22c66de719483d4
Thom Householder <thomhouseholder@gmail.com>
2015-12-07 15:01:27 -0500

    working on user authentication

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var passport = require("passport");
+var flash = require("connect-flash");
+var hbs = require("hbs");
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+mongoose.connect('mongodb://localhost/local-authentication-with-passport');
+
 var candidatesController = require("./controllers/candidatesController");
+var usersController = require("./controllers/usersController");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,6 +21,11 @@ app.use(bodyParser.urlencoded({extended: true}));
 //setting a view handler
 app.set("view engine", "hbs");
 
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+require('./config/passport')(passport);
 
 app.get('/', candidatesController.index);
 app.get('/candidates', candidatesController.index);
