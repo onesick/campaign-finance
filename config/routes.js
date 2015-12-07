1a3c372c497fe26fba42be84716baf4c00bc0fc5
Thom Householder <thomhouseholder@gmail.com>
2015-12-07 14:48:20 -0500

    created routes.js and signup.hbs

---
+++ config/routes.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+var usersController = require('../controllers/usersController');
+var candidatesController = require('../controllers/candidatesController');
